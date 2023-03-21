7cadfd3cfaaed2fa940c858f29b5f4678fcb7fe7
Jeff Greer <greergan@gmail.com>
2023-03-20 19:55:45 -0700

    mv slim.utilities import to https address

--- slim_modules.ts
+++ slim_modules.ts
@@ -1,5 +1,5 @@
 import { types } from "https://greergan.github.io/slim.types/index.ts";
-import * as utilities from "../slim.utilities/index.ts"
+import * as utilities from "https://greergan.github.io/slim.utilities/index.ts";
 export {
     types,
     utilities
